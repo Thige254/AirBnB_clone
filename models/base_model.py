a32907b9c25941a2caf6d5da0173845dbdd60188
Bappa-Kamba <bappahmuhammad18@gmail.com>
2023-10-12 15:00:56 +0100

    first iteration of console.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """The base model script"""
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -11,23 +12,36 @@ class BaseModel:
         """Initializes attributes for the BaseModel."""
         if kwargs:
             for key, value in kwargs.items():
+                # if `key` is `__class__`, skip it
+                if key == "__class__":
+                    continue
                 if key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    setattr(
+                        self, key, datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f"
+                        )
+                    )
                 else:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-
+            # set new instance in the `__objects` dict for storage
+            models.storage.new(self)
 
     def __str__(self):
         """String representation of BaseModel."""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__,
+            self.id,
+            self.__dict__
+        )
 
     def save(self):
         """Updates the updated_at attribute with the current datetime."""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Returns the dictionary representation of the BaseModel instance."""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,7 @@
 FileStorage class for AirBnB project
 """
 import json
+from models.base_model import BaseModel
 
 
 class FileStorage:
@@ -12,6 +13,7 @@ class FileStorage:
     """
     __file_path = "file.json"
     __objects = {}
+    __class_dict = {"BaseModel": BaseModel}
 
     def all(self):
         """Returns the dictionary __objects"""
@@ -24,7 +26,7 @@ class FileStorage:
 
     def save(self):
         """Serializes __objects to the JSON file (path: __file_path)"""
-        with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
+        with open(self.__file_path, 'w', encoding='utf-8') as f:
             obj_dict = {}
             for key, value in FileStorage.__objects.items():
                 obj_dict[key] = value.to_dict()
@@ -37,8 +39,11 @@ class FileStorage:
                 obj_dict = json.load(f)
                 for key, value in obj_dict.items():
                     class_name = value["__class__"]
-                    cls = eval(class_name)  # Convert string to class
-                    instance = cls(**value)  # Create a new instance
-                    FileStorage.__objects[key] = instance
+                    # Convert string to class
+                    cls = FileStorage.__class_dict.get(class_name)
+                    if cls:
+                        # Create a new instance
+                        instance = cls(**value)
+                        FileStorage.__objects[key] = instance
         except FileNotFoundError:
             pass
